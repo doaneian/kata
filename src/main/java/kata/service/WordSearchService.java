27c58811891d7678946c4161cd6a7ef33f845f56
doaneian <ian.doane@gmail.com>
2020-02-04 19:21:04 -0600

    get horizontal forward check working

--- src/main/java/kata/service/WordSearchService.java
+++ src/main/java/kata/service/WordSearchService.java
@@ -1,8 +1,43 @@
 package kata.service;
 
+import kata.bo.Coordinate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class WordSearchService {
-    public void searchWords(String[] words, String[][] puzzle) {}
+    public Map<String, List<Coordinate>> searchWords(String[] words, String[][] puzzle) {
+        Map<String, List<Coordinate>> solution = new HashMap<>();
+        for (int i = 0; i < puzzle.length; i++) {
+            String[] row = puzzle[i];
+            for (int j = 0; j < row.length; j++) {
+                for (String word: words) {
+                    if (row[j].compareTo(word.substring(0, 1)) == 0) {
+                        List<Coordinate> possibleCoordinates = checkHorizontalForward(row, j, i, word);
+                        if (possibleCoordinates.size() == word.length()) {
+                            solution.put(word, possibleCoordinates);
+                        }
+                    }
+                }
+            }
+        }
+
+        return solution;
+    }
+
+    private List<Coordinate> checkHorizontalForward(String[] row, int startIndex, int rowIndex, String word) {
+        List<Coordinate> coordinates = new ArrayList<>();
+        for (int i = startIndex; i < row.length && i < word.length() &&
+                row[i].compareTo(word.substring(i - startIndex, i - startIndex + 1)) == 0; i++) {
+            Coordinate coordinate = new Coordinate();
+            coordinate.setX(i);
+            coordinate.setY(rowIndex);
+            coordinates.add(coordinate);
+        }
+        return coordinates;
+    }
 }
---
+++ src/test/java/kata/service/WordSearchServiceTest.java
@@ -0,0 +1,37 @@
+package kata.service;
+
+import kata.bo.Coordinate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class WordSearchServiceTest {
+    WordSearchService wordSearchService;
+
+    @Before
+    public void init() {
+        this.wordSearchService = new WordSearchService();
+    }
+
+    @Test
+    public void testFindOneWordHorizontally() {
+        Map<String, List<Coordinate>> solution = wordSearchService.searchWords(new String[]{"test"},
+                new String[][]{{"t", "e", "s", "t"}, {"a", "b", "c", "d"}, {"e", "f", "g", "h"}, {"i", "j", "k", "l"}});
+        Assert.assertEquals(solution.get("test").size(), 4);
+
+        Assert.assertEquals(solution.get("test").get(0).getX(), 0);
+        Assert.assertEquals(solution.get("test").get(0).getY(), 0);
+
+        Assert.assertEquals(solution.get("test").get(1).getX(), 1);
+        Assert.assertEquals(solution.get("test").get(1).getY(), 0);
+
+        Assert.assertEquals(solution.get("test").get(2).getX(), 2);
+        Assert.assertEquals(solution.get("test").get(2).getY(), 0);
+
+        Assert.assertEquals(solution.get("test").get(3).getX(), 3);
+        Assert.assertEquals(solution.get("test").get(3).getY(), 0);
+    }
+}
